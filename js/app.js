3adbe2b90ebf161b0df595a4d9aa15cdcf5be4fa
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-05-02 12:40:38 +0600

    Create handleClick function

--- js/app.js
+++ js/app.js
@@ -1,24 +1,16 @@
-let display = document.getElementById('input'),
-		ce = document.getElementById('ce'),
-		c = document.getElementById('c'),
-		numbers = document.querySelectorAll('numbers'),
+let d = document,
+		display = d.getElementById('input'),
+		ce = d.getElementById('ce'),
+		c = d.getElementById('c'),
+		numbers = d.querySelectorAll('numbers'),
 		operations = document.querySelectorAll('operation'),
-		decimal = document.getElementById('decimal'),
-		result = document.getElementById('result');
+		decimal = d.getElementById('decimal'),
+		result = d.getElementById('result');
 
-decimal.addEventListener('click', () => {
-	console.log('You clicked decimal');
-});
-c.addEventListener('click', () => {
-	console.log('You clicked C');
-});
-ce.addEventListener('click', () => {
-	console.log('You clicked CE');
-});
-result.addEventListener('click', () => {
-	console.log('You clicked result');
-});
-display.addEventListener('input', () => {
-	console.log('You focused on input');
-});
+let handleClick = (e) => {
+	let value = e.target.textContext;
+	display.value = value;
+}
+
+
 
