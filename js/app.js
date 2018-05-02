40738f69e1b94d7a0ab6ace15aa6ee91158f7236
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-05-02 12:54:42 +0600

    Remove specific variables for buttons and create btns, d variables

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 let d = document,
-		display = d.getElementById('input'),
-		ce = d.getElementById('ce'),
-		c = d.getElementById('c'),
-		numbers = d.querySelectorAll('numbers'),
-		operations = document.querySelectorAll('operation'),
-		decimal = d.getElementById('decimal'),
-		result = d.getElementById('result');
+		btns = d.querySelectorAll('.btn'),
+		display = d.getElementById('input');
+
+console.log(btns);
 
 let handleClick = (e) => {
 	let value = e.target.textContext;
 	display.value = value;
-}
+};
 
+for (let i = 0; i < btns.length; i++) {
+	let btn = btns[i];
+
+	btn.addEventListener('click', handleClick);
+};
 
 
