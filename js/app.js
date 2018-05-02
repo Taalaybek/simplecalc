dbad0b038b97aee239f913ac75d77a82a1229c45
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-05-02 18:25:16 +0600

    Create clear_btn class for ce, c buttons. Delete ce, c variables. Create eventListener for clear function

--- js/app.js
+++ js/app.js
@@ -1,44 +1,62 @@
 let d = document,
-		c = d.getElementById('c'),
-		ce = d.getElementById('ce'),
-		btns = d.querySelectorAll('.btn'),
 		display = d.getElementById('input'),
 		result = d.getElementById('result'),
 		numbers = d.querySelectorAll('.number'),
 		dotButton = d.getElementById('decimal'),
+		clearBtns = d.querySelectorAll('.clear_btn'),
 		operations = d.querySelectorAll('.operation');
 
-let handleClick = (e) => {
-	let value = e.target.innerText;
-	display.value = value;
+// Event listener for numbers buttons
+for (let i = 0; i < numbers.length; i++) {
+	let number = numbers[i];
+
+	number.addEventListener('click', () => {
+		console.log(number);
+	});
 };
+
+// Event listener for operations buttons
+for (let i = 0; i < operations.length; i++) {
+	let operationBtn = operations[i];
+	
+	operationBtn.addEventListener('click', () => {
+		console.log('operation');
+	});
+}
+
+// Event listener for clear buttons
+for (let i = 0; i < clearBtns.length; i++) {
+	let clearBtn = clearBtns[i];
+	clearBtn.addEventListener('click', (e) => {
+		clear(e.srcElement.id);
+	});
+}
+
+dotButton.addEventListener('click', (e) => {
+	console.log(e.srcElement.id);
+});
+result.addEventListener('click', (e) => {
+	console.log(e.srcElement.id);
+});
 
 let numberPress = () => {
-	for (let i = 0; i < numbers.length; i++) {
-		let number = numbers[i];
-	
-		number.addEventListener('click', handleClick);
-	};
+	// body ...
 };
-numberPress();
 
 let operation = () => {
-	for (let i = 0; i < operations.length; i++) {
-		let operation = operations[i];
-		
-		operation.addEventListener('click', handleClick);
-	}
+	// body ...
 };
-operation();
 
 let decimal = () => {
-	dotButton.addEventListener('click', handleClick);
+	// body ...
 };
-decimal();
 
-let clear = () => {
-
+let clear = (id) => {
+	if (id === 'ce') {
+		console.log("Cut symbol at the end!");
+	}else {
+		display.value = "";
+	}
 };
-clear();
 
 
