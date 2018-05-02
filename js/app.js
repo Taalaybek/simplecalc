69501193fb87972a165450925467affb5f6b24b8
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-05-02 15:57:31 +0600

    Add classes for numbers, operations buttons for using in js events

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 let d = document,
+		c = d.getElementById('c'),
+		ce = d.getElementById('ce'),
 		btns = d.querySelectorAll('.btn'),
-		display = d.getElementById('input');
-
-let handleClick = (e) => {
-	let value = e.target.innerText;
-	display.value = value;
-};
+		display = d.getElementById('input'),
+		result = d.getElementById('result'),
+		numbers = d.querySelectorAll('.number'),
+		dotButton = d.getElementById('.decimal'),
+		operations = d.querySelectorAll('.operation');
 
 let numberPress = () => {
+	let handleClick = (e) => {
+		let value = e.target.innerText;
+		display.value = value;
+	};
+	
 	for (let i = 0; i < btns.length; i++) {
 		let btn = btns[i];
 	
